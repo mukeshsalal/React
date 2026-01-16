623d34eea8f227faf134c793be25b71bc7a8e8c7
mukeshsalal <salalmukesh06@gmail.com>
2026-01-16 23:00:05 +0530

    Learning

---
+++ DAY2/react-props/Components/Product.jsx
@@ -0,0 +1,15 @@
+
+
+import "./Product.css";
+
+function Product(){
+    return(
+<div>
+    <h1>Product Title</h1>
+    <h5>Product Description</h5>
+</div>
+
+    );
+}
+
+export default Product;
---
+++ DAY2/react-props/Components/ProductTab.jsx
@@ -0,0 +1,17 @@
+
+
+
+import "./ProductTab.css";
+import Product from "./Product.jsx";
+
+function ProductTab(){
+    return(
+<div>
+    <Product/>
+    <Product/>
+    <Product/>
+</div>
+    );
+}
+
+export default ProductTab;
